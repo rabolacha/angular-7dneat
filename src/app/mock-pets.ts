cc9a7a8c3dc7400948a3d5b78dc39e755c48bbcb
rabolacha <rachelsanchan@gmail.com>
2021-03-07 22:24:09 +0000

    1 file modified

--- src/app/mock-pets.ts
+++ src/app/mock-pets.ts
@@ -1,10 +1,9 @@
 import { Pet } from './pet';
 
 export const PETS: Pet[] = [
-  { id: 1, name: 'NiceNice' },
+  { id: 1, name: 'Wilson' },
   { id: 2, name: 'Nox' },
-  { id: 3, name: 'estiBomba' },
+  { id: 3, name: 'esti bomba' },
   { id: 4, name: 'Chef De Chef' },
-  { id: 5, name: 'Magneta' },
   { id: 6, name: 'OceanMan' }, 
 ];
